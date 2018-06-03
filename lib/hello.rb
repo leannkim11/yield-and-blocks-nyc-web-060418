04f4f4e1262764afb9f7bd23d3472a3733226c5b
Leann Kim <leannminakim@gmail.com>
2018-06-03 21:12:01 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,12 @@
-def hello_t (array)
+def hello_t(array)
   i = 0
-
+ 
   while i < array.length
+    yield(array[i])
     i = i + 1
-    yield array[i]
   end
+ 
+  array
 end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
